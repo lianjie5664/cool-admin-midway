16219ed97a19b7f72c1dc9a774c77ff53ed24fb6
5664 <takehands@163.com>
2021-04-13 10:18:48 +0800

    4.13 远程拉取

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -10,7 +10,7 @@ export default (appInfo: EggAppInfo) => {
     host: '127.0.0.1',
     port: 3306,
     username: 'root',
-    password: '123123',
+    password: '123456',
     database: 'cool',
     // 自动建表 注意：线上部署的时候不要使用，有可能导致数据丢失
     synchronize: true,
